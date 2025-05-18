0523b5f41bc19f6d2d9e9894e82005dea4e7d093
ehsanmehrjoo <ehsan585147z@gmail.com>
2025-05-19 00:21:39 +0330

    Add multilingual support with auto language detection based on user location (fa, en, de)

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,40 +1,24 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+
 import en from '../locales/en/translation.json'
 import fa from '../locales/fa/translation.json'
-import de from '../locales/de/translation.json'
+import de from '../locales/de/translation.json'  // 🔹 اضافه‌شده
 
-const detectLanguageFromCountry = async () => {
-  try {
-    const res = await fetch('https://ipapi.co/json/')
-    const data = await res.json()
-    const country = data.country_code
-
-    if (country === 'IR') return 'fa'
-    if (country === 'DE') return 'de'
-    return 'en'
-  } catch (err) {
-    return 'en'
-  }
-}
-
-export const setupI18n = async () => {
-  const detectedLang = await detectLanguageFromCountry()
-
-  await i18n
-    .use(initReactI18next)
-    .init({
-      resources: {
-        en: { translation: en },
-        fa: { translation: fa },
-        de: { translation: de },
-      },
-      lng: detectedLang,
-      fallbackLng: 'en',
-      interpolation: {
-        escapeValue: false,
-      },
-    })
-}
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: en },
+      fa: { translation: fa },
+      de: { translation: de },  
+    },
+    fallbackLng: 'en', 
+    interpolation: {
+      escapeValue: false,
+    },
+  })
 
 export default i18n
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { setupI18n } from './i18n'
-import './index.css'
 
-setupI18n().then(() => {
-  ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
-})
+import './index.css'
+import './i18n'
+import React from 'react'
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
